ffe869d3f1c2d2aa5ceb7a50e7da6f4d4d1232f6
maiduyquan <91303493+maiduyquan@users.noreply.github.com>
2023-03-09 23:50:31 +0700

    baithi

---
+++ HRM_Humman/HRM_Humman/Models/EmployeeContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRM_Humman.Models;
+
+public class EmployeeContext : DbContext
+{
+    public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Employee> Employees { get; set; } = null!;
+}
